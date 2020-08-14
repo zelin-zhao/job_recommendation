e12ed9c53f93c8b42fae8a96302a0556c4bf11c1
Zelin Zhao <zzhao36@cs.uchicago.edu>
2020-08-13 22:43:14 -0500

    implemented favorite servlet(/history), service and function for database read/update

--- src/main/java/rpc/SearchItem.java
+++ src/main/java/rpc/SearchItem.java
@@ -2,6 +2,7 @@ package rpc;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
+import db.MySQLConnection;
 import entity.Item;
 import external.GithubClient;
 
@@ -34,12 +37,19 @@ public class SearchItem extends HttpServlet {
 		// TODO Auto-generated method stub
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
+		String userId = request.getParameter("user_id");
+		
+		MySQLConnection connection = new MySQLConnection();
+		Set<String> favoritedItemIds = connection.getFavoriteItemIds(userId);
+		connection.close();
 
 		GithubClient client = new GithubClient();
 		List<Item> items = client.search(lat, lon, null);
 		JSONArray array = new JSONArray();
 		for (Item item : items) {
-			array.put(item.toJSONObject());
+			JSONObject obj = item.toJSONObject();
+			obj.put("favorite", favoritedItemIds.contains(item.getItemId()));
+			array.put(obj);
 		}
 		RpcHelper.writeJsonArray(response, array);
 	}
