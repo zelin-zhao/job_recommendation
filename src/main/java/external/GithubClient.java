698aeb4873b1a478311289e5489161841a3edab0
Zelin Zhao <zzhao36@cs.uchicago.edu>
2020-08-21 06:35:40 -0500

    Extracted keywords for recommendation

--- src/main/java/external/GithubClient.java
+++ src/main/java/external/GithubClient.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
@@ -23,7 +24,7 @@ import entity.Item;
 public class GithubClient {
 	private static final String URL_TEMPLATE = "https://jobs.github.com/positions.json?description=%s&lat=%s&long=%s";
 	private static final String DEFAULT_KEYWORD = "developer";
-	
+
 	public List<Item> search(double lat, double lon, String keyword) {
 		if (keyword == null) {
 			keyword = DEFAULT_KEYWORD;
@@ -34,28 +35,28 @@ public class GithubClient {
 			e.printStackTrace();
 		}
 		String url = String.format(URL_TEMPLATE, keyword, lat, lon);
-		
+
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpGet httpget = new HttpGet(url);
 		// Create a custom response handler
-	    ResponseHandler<List<Item>> responseHandler = new ResponseHandler<List<Item>>() {
+		ResponseHandler<List<Item>> responseHandler = new ResponseHandler<List<Item>>() {
 
-	        @Override
-	        public List<Item> handleResponse(final HttpResponse response) throws ClientProtocolException, IOException {
-	            int status = response.getStatusLine().getStatusCode();
-	            if (status != 200) {
-	                return new ArrayList<>();
-	            }
-	            HttpEntity entity = response.getEntity();
-	            if (entity == null) {
-	            	return new ArrayList<>();
-	            }
-	            String responseBody = EntityUtils.toString(entity);
-	            JSONArray array = new JSONArray(responseBody);
-	            return getItemList(array);
-	        }
+			@Override
+			public List<Item> handleResponse(final HttpResponse response) throws ClientProtocolException, IOException {
+				int status = response.getStatusLine().getStatusCode();
+				if (status != 200) {
+					return new ArrayList<>();
+				}
+				HttpEntity entity = response.getEntity();
+				if (entity == null) {
+					return new ArrayList<>();
+				}
+				String responseBody = EntityUtils.toString(entity);
+				JSONArray array = new JSONArray(responseBody);
+				return getItemList(array);
+			}
 
-	    };
+		};
 
 		try {
 			return httpclient.execute(httpget, responseHandler);
@@ -67,26 +68,36 @@ public class GithubClient {
 
 		return new ArrayList<>();
 	}
-	
+
 	private List<Item> getItemList(JSONArray array) {
 		List<Item> itemList = new ArrayList<>();
+		List<String> descriptionList = new ArrayList<>();
+
+		for (int i = 0; i < array.length(); i++) {
+			String description = getStringFieldOrEmpty(array.getJSONObject(i), "description");
+			if (description.equals("") || description.equals("\n")) {
+				descriptionList.add(getStringFieldOrEmpty(array.getJSONObject(i), "title"));
+			} else {
+				descriptionList.add(description);
+			}
+		}
+
+		List<List<String>> keywords = MonkeyLearnClient
+				.extractKeywords(descriptionList.toArray(new String[descriptionList.size()]));
+
 		for (int i = 0; i < array.length(); i++) {
 			JSONObject object = array.getJSONObject(i);
-			Item item = Item.builder()
-							.itemId(getStringFieldOrEmpty(object, "id"))
-							.name(getStringFieldOrEmpty(object, "title"))
-							.address(getStringFieldOrEmpty(object, "location"))
-							.url(getStringFieldOrEmpty(object, "url"))
-							.imageUrl(getStringFieldOrEmpty(object, "company_logo"))
-							.build();
+			Item item = Item.builder().itemId(getStringFieldOrEmpty(object, "id"))
+					.name(getStringFieldOrEmpty(object, "title")).address(getStringFieldOrEmpty(object, "location"))
+					.url(getStringFieldOrEmpty(object, "url")).imageUrl(getStringFieldOrEmpty(object, "company_logo"))
+					.keywords(new HashSet<String>(keywords.get(i))).build();
 			itemList.add(item);
 		}
 		return itemList;
 	}
-	
+
 	private String getStringFieldOrEmpty(JSONObject obj, String field) {
 		return obj.isNull(field) ? "" : obj.getString(field);
 	}
 
-	
 }
